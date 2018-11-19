709ce29d80748842a4f0ba489a1a080bfd7b73b7
Rapter <sngermiyanoglu@hotmail.com>
2018-11-19 10:07:32 +0300

    Add files via upload

---
+++ 17-node.js baslangic kaynak/1-Hello World/index.js
@@ -0,0 +1,8 @@
+// 1-Hello World içinde olduğundan cd yazıp tab basarak getirme => cd '.\1-Hello World\'
+// node index.js çalıştırma
+console.log("Hello World");
+
+const a = 10;
+const b = 20;
+
+console.log(a+b);
---
+++ 17-node.js baslangic kaynak/2-Paket Kullanımı/index.js
@@ -0,0 +1,6 @@
+// Paketi Dahil etme
+const is = require("simple-is");
+
+console.log(is.number(24)); // true
+
+console.log(is.string(24)); // false
